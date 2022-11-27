b774861be95f83ee0c821a20eb459a9ad1a63a02
alexe <prograbota@mail.ru>
2022-11-28 00:31:15 +0300

    Initial commit

---
+++ src/test/java/ru/spiridonov/pp316/Pp316ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.spiridonov.pp316;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pp316ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
